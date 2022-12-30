4725f6b2b8268769f71937e678e965bd87de5067
mayankk7 <mayank26032@gmail.com>
2022-12-30 16:22:28 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {motion} from "framer-motion"
+import {useRef, useEffect, useState} from "react"
+import data from "./data"
 
 function App() {
+  //console.log(data)
+  const [width,setwidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(()=>{
+    //console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setwidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  },[width])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Carousel</h1>
+      <motion.div ref={carousel} className='carousel' whileTap={{cursor: "grabbing"}}>
+        <motion.div drag="x" 
+         dragConstraints={{right: 0, left: -width }}
+         className='inner-carousel'>
+            {
+              data.map((e)=>{
+                return (
+                  <motion.div 
+                  className='item'       
+                  initial={{ opacity: 0, scale: 0.3 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.2 }}>
+                  <motion.div className='card'>
+                  <motion.img src={e.img}></motion.img>
+                      <p>{e.title}</p>
+                      <p>{e.price}</p>
+                    
+                  </motion.div>                
+                  </motion.div>
+                )
+              })
+            }
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        id: 0,
+        title: "Hitman 3 PS5",
+        img: "https://static.wixstatic.com/media/621e45_6ec3df928bc54e518957e28b297b6706~mv2.jpeg",
+        price: "Rs.1999"
+    },
+    {
+        id: 1,
+        title: "Tony Hawk Pro Skater 1+2 PS4",
+        img: "https://static.wixstatic.com/media/621e45_d1d57a3fe5344234aa419330df4a5ec7~mv2.jpeg",
+        price: "Rs.1499"
+    },
+    {
+        id: 2,
+        title: "Ghost of Tsushima PS5",
+        img: "https://static.wixstatic.com/media/621e45_696fd473d04c424780a4833ddcfb3362~mv2.jpeg",
+        price: "Rs.1799"
+    },
+    {
+        id: 3,
+        title: "Star Wars Jedi: Fallen Order PS4",
+        img: "https://static.wixstatic.com/media/621e45_8e29029616214310bcdcaf5495c68511~mv2.jpeg",
+        price:"1299"
+    },
+    {
+        id: 4,
+        title: "Sniper Elite 5",
+        img: "https://static.wixstatic.com/media/621e45_4c28c33fccf94e5483117be38aae393e~mv2.jpg",
+        price: "Rs. 2499"
+    },
+    {
+        id: 5,
+        title: "SpiderMan: Miles Morales",
+        img: "https://static.wixstatic.com/media/621e45_2844e30e30c9422eb1d483b14cb4b7e7~mv2.jpeg",
+        price: "Rs.1499"
+    },
+    {
+        id: 6,
+        title: "UFC 4 PS4",
+        img: "https://static.wixstatic.com/media/621e45_e9c03abeea4640b791ddc3cb1672c1c8~mv2.jpeg",
+        price: "Rs.1399"
+    },
+    {
+        id: 7,
+        title: "Horizon Forbidden",
+        img: "https://static.wixstatic.com/media/621e45_4a8c0586ebd54d3fb7a97dd026ad3755~mv2.jpg",
+        price: "Rs.1499"
+    }
+]
+
+
+export default data
